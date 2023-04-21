eb1fc3786ac0098d06809db5cdc519b2a9f1d01b
Geetha Charan Reddy <cgeetacharan@gmail.com>
2023-04-21 16:45:08 +0530

    add args

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,9 +1,14 @@
 import apache_beam as beam
 from apache_beam import Pipeline
 
-with Pipeline() as p:
-    (
-        p 
-        | "Initalize" >> beam.Create([{"test": 1}, {"test": 2}])
-        | "Print" >> beam.Map(print)
-    )
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", default='testing param', help='this is a testing param')
+    args, kwargs = parser.parse_known_args()
+    with Pipeline() as p:
+        (
+            p 
+            | "Initalize" >> beam.Create([{"test": 1}, {"test": 2}, {"test": args}])
+            | "Print" >> beam.Map(print)
+        )
