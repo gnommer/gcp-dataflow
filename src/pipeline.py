6f191c1dfa34b2f482f5c74ad985e925d352596f
Geetha Charan Reddy <cgeetacharan@gmail.com>
2023-04-21 21:35:03 +0530

    change options

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,12 +1,14 @@
 import apache_beam as beam
 from apache_beam import Pipeline
+from apache_beam.pipeline_options import PipelineOptions
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--test", default='testing param', help='this is a testing param')
     args, kwargs = parser.parse_known_args()
-    with Pipeline(streaming=False) as p:
+    pipeline_options = PipelineOptions(streaming=False, save_main_session=True)
+    with Pipeline(options=pipeline_options) as p:
         (
             p 
             | "Initalize" >> beam.Create([{"test": 1}, {"test": 2}, {"test": args}])
